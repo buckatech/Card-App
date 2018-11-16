bce1bd99bb308e07fc3a55571cec450a156b0aef
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-11-16 10:21:59 -0700

    added login/register modal, use of bootstrap

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: "bubbles",
+      password: "bubbles1234",
+      database: "bubbles",
+      charset: "utf8"
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
+
